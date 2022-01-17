88bf5fc0296b292be19ab239e74da5ae72c741c2
Samuel Gauthier <samgau1997@gmail.com>
2022-01-17 11:48:07 -0500

    Add files via upload

---
+++ test2.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", function(req, res){
+	res.send("Hello World!");
+})
+
+app.listen(process.env.PORT || 5000);
+module.exports = app;
