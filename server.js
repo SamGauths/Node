4e375175f43b4320ab5cc17bcf3dfbb6c2f22c87
Samuel Gauthier <samgau1997@gmail.com>
2022-01-31 20:10:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,9 @@
-import fetch from "node-fetch";
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 
-app.get("/", function(req, res){
-  let queryParams = `{
-  token_erc20 (where :{
-      _and:[
-        {contract_address:
-          {_eq:"io1zl0el07pek4sly8dmscccnm0etd8xr8j02t4y7"}}
-        {recipient:
-          {_eq:"io1cz340sadrx0zumau3e9vms8ulcy3kuguljcevt"}}
-        {timestamp:{_gt:"2022-01-9T00:00:00"}}
-      ]
-    }
-    )
-    {
-      amount
-    }
-}`;
   
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
