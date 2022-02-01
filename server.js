03b96bcd70fdc7890bcdce6c0f2dd0049b3e7bda
Samuel Gauthier <samgau1997@gmail.com>
2022-01-31 19:57:52 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+
+app.get("/", function(req, res){
+    res.send("Hello World!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("Sup!");
+});
